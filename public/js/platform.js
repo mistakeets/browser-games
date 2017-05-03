2bb6c265844b7ac51ccb2d607ec496149c1848d9
Keith Oppel <mistakeets@gmail.com>
2017-05-03 13:29:55 -0700

    finishes implementation of drawing/rendering the game level

--- public/js/platform.js
+++ public/js/platform.js
@@ -96,3 +96,86 @@ Coin.prototype.type = 'coin'
 
 let completeLevel = new Level(levelPlan)
 console.log(completeLevel.width, 'by', completeLevel.height)
+
+// DRAWING functionality
+// TODO: make this modular? / split level creation and drawing into separate files?
+
+function elt(name, className) {
+  let elt = document.createElement(name)
+  if (className) elt.className = className
+  return elt
+}
+
+function DOMdisplay(parent, level) {
+  this.wrap = parent.appendChild(elt('div', 'game'))
+  this.level = level
+
+  this.wrap.appendChild(this.drawBackground())
+  this.elementLayer = null
+  this.drawFrame()
+}
+
+const scale = 20
+
+DOMdisplay.prototype.drawBackground = () => {
+  let table = elt('table', 'background')
+  table.style.width = this.level.width * scale + 'px'
+  this.level.grid.forEach((row) => {
+    let rowElt = table.appendChild(elt('tr'))
+    rowElt.style.height = scale + 'px'
+    row.Foreach((type) => {
+      row.appendChild(elt('td', type))
+    })
+  })
+  return table
+}
+
+DOMdisplay.prototype.drawElements = () => {
+  let wrap = elt('div')
+  this.level.elements.forEach((element) => {
+    let rect = wrap.appendChild(elt('div', element + element.type))
+    rect.style.width = element.size.x * scale + 'px'
+    rect.style.height = element.size.y * scale + 'px'
+    rect.style.left = element.position.x * scale + 'px'
+    rect.style.top = element.position.y * scale + 'px'
+  })
+  return wrap
+}
+
+DOMdisplay.prototype.drawFrame = () => {
+  if (this.elementLayer)
+    this.wrap.removeChild(this.elementLayer)
+  this.elementLayer = this.wrap.appendChild(this.drawElements())
+  this.wrap.className = 'game' + (this.level.status || '')
+  this.scrollPlayerIntoView
+}
+
+DOMdisplay.prototype.scrollPlayerIntoView = () => {
+  let width = this.wrap.clientWidth
+  let height = this.wrap.clientHeight
+  let margin = width / 3
+
+  let left = this.wrap.scrollLeft,
+    right = left + width
+  let top = this.wrap.scrollTop,
+    bottom = top + height
+
+  let player = this.level.player
+  let center = player.position.plus(player.size.times(0.5))
+    .times(scale)
+
+  if (center.x < left + margin) {
+    this.wrap.scrollLeft = center.x - margin
+  } else if (center.x > right - margin) {
+    this.wrap.scrollLeft = center.x + margin - width
+  }
+  if (center.y < top.margin) {
+    this.wrap.scrollTop = center.y - margin
+  } else if (center.y > bottom - margin) {
+    this.wrap.scrollTop = center.y + margin - height
+  }
+}
+
+DOMdisplay.prototype.clear = () => {
+  this.wrap.parentNode.removeChild(this.wrap)
+}
