dc7db03ed610d8f69563a40baaa9b27bebb1fa86
Keith Oppel <mistakeets@gmail.com>
2017-05-03 10:28:46 -0700

    finishes implementation of game level elements

--- public/js/platform.js
+++ public/js/platform.js
@@ -28,15 +28,71 @@ for (let y = 0; y < this.height; y++) {
     let Element = elementCharacters[char]
     if (Element) {
       this.elements.push(new Element(new Vector(x, y), char))
-    } else if (char == "x") {
-      fieldtype = "wall"
-    } else if (char == "!") {
-      fieldtype = "lava"
+    } else if (char == 'x') {
+      fieldtype = 'wall'
+    } else if (char == '!') {
+      fieldtype = 'lava'
       gridLine.push(gridLine)
     }
   }
   this.player = this.elements.filter((element) => {
-    return element.type == "player"
+    return element.type == 'player'
   })[0]
   this.status = this.finishDelay = null
 }
+
+function Vector(x, y) {
+  this.x = x
+  this.y = y
+}
+
+Level.prototype.isFinished = () => {
+  return this.status != null && this.finishDelay < 0
+}
+
+Vector.prototype.plus = (other) => {
+  return new Vector(this.x + other.x, this.y + other.y)
+}
+
+Vector.prototype.times = (factor) => {
+  return new Vector(this.x * factor, this.y * factor)
+}
+
+const elementCharacters = {
+  '@': Player,
+  'o': Coin,
+  '=': Lava,
+  '|': Lava,
+  'v': Lava
+}
+
+function Player(position) {
+  this.position = position.plus(new Vector(0, -0.5))
+  this.size = new Vector(0.8, 1.5)
+  this.speed = new Vector(0, 0)
+}
+Player.prototype.type = 'player'
+
+function Lava(postion, char) {
+  this.position = position
+  this.size = new Vector(1, 1)
+  if (char == '=') {
+    this.speed = new Vector(2, 0)
+  } else if (char == '|') {
+    this.speed = new Vector(0, 2)
+  } else if (char == 'v') {
+    this.speed = new Vector(0, 3)
+    this.repeatPosition = position
+  }
+}
+Lava.prototype.type = 'lava'
+
+function Coin(position) {
+  this.basePosition = this.position = position.plus(new Vector(0.2, 0.1))
+  this.size = new Vector(0.6, 0.6)
+  this.wobble = Math.random() * Math.PI * 2
+}
+Coin.prototype.type = 'coin'
+
+let completeLevel = new Level(levelPlan)
+console.log(completeLevel.width, 'by', completeLevel.height)
