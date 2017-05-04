41dae35776e2c06106401b3bf1f265607683b571
Keith Oppel <mistakeets@gmail.com>
2017-05-04 13:50:50 -0700

    updates game to use canvas for drawing the game objects

--- public/js/platform_dom_version.js
+++ public/js/platform_dom_version.js
@@ -98,142 +98,85 @@ var simpleLevel = new Level(simpleLevelPlan)
 
 // Drawing Functionality
 
+function elt(name, className) {
+  var elt = document.createElement(name)
+  if (className) elt.className = className
+  return elt
+}
+
+function DOMDisplay(parent, level) {
+  this.wrap = parent.appendChild(elt('div', 'game'))
+  this.level = level
+
+  this.wrap.appendChild(this.drawBackground())
+  this.actorLayer = null
+  this.drawFrame()
+}
+
 var scale = 20
 
-function flipHorizontally(context, around) {
-  context.translate(around, 0);
-  context.scale(-1, 1);
-  context.translate(-around, 0);
+DOMDisplay.prototype.drawBackground = function() {
+  var table = elt('table', 'background')
+  table.style.width = this.level.width * scale + 'px'
+  this.level.grid.forEach(function(row) {
+    var rowElt = table.appendChild(elt('tr'))
+    rowElt.style.height = scale + 'px'
+    row.forEach(function(type) {
+      rowElt.appendChild(elt('td', type))
+    })
+  })
+  return table
 }
 
-function CanvasDisplay(parent, level) {
-  this.canvas = document.createElement('canvas')
-  this.canvas.width = Math.min(600, level.width * scale)
-  this.canvas.height = Math.min(450, level.height * scale)
-  parent.appendChild(this.canvas)
-  this.ctx = this.canvas.getContext('2d')
-
-  this.level = level
-  this.animationTime = 0
-  this.flipPlayer = false
-
-  this.viewport = {
-    left: 0,
-    top: 0,
-    width: this.canvas.width / scale,
-    height: this.canvas.height / scale
-  }
-
-  this.drawFrame(0)
-
+DOMDisplay.prototype.drawActors = function() {
+  var wrap = elt('div')
+  this.level.actors.forEach(function(actor) {
+    var rect = wrap.appendChild(elt('div',
+      'actor ' + actor.type))
+    rect.style.width = actor.size.x * scale + 'px'
+    rect.style.height = actor.size.y * scale + 'px'
+    rect.style.left = actor.position.x * scale + 'px'
+    rect.style.top = actor.position.y * scale + 'px'
+  })
+  return wrap
 }
 
-CanvasDisplay.prototype.clear = function() {
-  this.canvas.parentNode.removeChild(this.canvas)
+DOMDisplay.prototype.drawFrame = function() {
+  if (this.actorLayer)
+    this.wrap.removeChild(this.actorLayer)
+  this.actorLayer = this.wrap.appendChild(this.drawActors())
+  this.wrap.className = 'game ' + (this.level.status || '')
+  this.scrollPlayerIntoView()
 }
 
-CanvasDisplay.prototype.drawFrame = function(step) {
-  this.animationTime += step
+DOMDisplay.prototype.scrollPlayerIntoView = function() {
+  var width = this.wrap.clientWidth
+  var height = this.wrap.clientHeight
+  var margin = width / 3
 
-  this.updateViewport()
-  this.clearDisplay()
-  this.drawBackground()
-  this.drawActors()
-}
+  // The viewport
+  var left = this.wrap.scrollLeft
+  var right = left + width
+  var top = this.wrap.scrollTop
+  var bottom = top + height
 
-CanvasDisplay.prototype.updateViewport = function() {
-  var view = this.viewport
-  var margin = view.width / 3
   var player = this.level.player
   var center = player.position.plus(player.size.times(0.5))
+    .times(scale);
 
-  if (center.x < view.left + margin) {
-    view.left = Math.max(center.x - margin, 0)
-  } else if (center.x > view.left + view.width - margin) {
-    view.left = Math.min(center.x + margin - view.width, this.level.width - view.width)
-  }
-  if (center.y < view.top + margin) {
-    view.top = Math.max(center.y - margin, 0)
-  } else if (center.y > view.top + view.height - margin) {
-    view.top = Math.min(center.y + margin - view.height, this.level.height - view.height)
-  }
+  if (center.x < left + margin)
+    this.wrap.scrollLeft = center.x - margin
+  else if (center.x > right - margin)
+    this.wrap.scrollLeft = center.x + margin - width
+  if (center.y < top + margin)
+    this.wrap.scrollTop = center.y - margin
+  else if (center.y > bottom - margin)
+    this.wrap.scrollTop = center.y + margin - height
 }
 
-CanvasDisplay.prototype.clearDisplay = function() {
-  if (this.level.status == 'won') {
-    this.ctx.fillStyle = 'rgb(68, 191, 255)'
-  } else if (this.level.status == 'lost') {
-    this.ctx.fillStyle = 'rgb(44, 136, 214)'
-  } else {
-    this.ctx.fillStyle = 'rgb(52, 166, 251)'
-  }
-  this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height)
+DOMDisplay.prototype.clear = function() {
+  this.wrap.parentNode.removeChild(this.wrap)
 }
-
-var otherSprites = document.createElement('img')
-otherSprites.src = 'img/sprites.png'
-
-CanvasDisplay.prototype.drawBackground = function() {
-  var view = this.viewport
-  var xStart = Math.floor(view.left)
-  var xEnd = Math.ceil(view.left + view.width)
-  var yStart = Math.floor(view.top)
-  var yEnd = Math.ceil(view.top + view.height)
-
-  for (var y = yStart; y < yEnd; y++) {
-    for (var x = xStart; x < xEnd; x++) {
-      var tile = this.level.grid[y][x]
-      if (tile == null) continue
-      var screenX = (x - view.left) * scale
-      var screenY = (y - view.top) * scale
-      var tileX = tile == "lava" ? scale : 0
-      this.ctx.drawImage(otherSprites, tileX, 0, scale, scale,
-        screenX, screenY, scale, scale)
-    }
-  }
-}
-
-var playerSprites = document.createElement('img')
-playerSprites.src = 'img/player1.png'
-var playerXOverlap = 4
-
-CanvasDisplay.prototype.drawPlayer = function(x, y, width, height) {
-  var sprite = 8
-  var player = this.level.player
-  width += playerXOverlap * 2
-  x -= playerXOverlap
-  if (player.speed.x != 0) {
-    this.flipPlayer = player.speed.x < 0
-  }
-  if (player.speed.y != 0) {
-    sprite = 9
-  } else if (player.speed.x != 0) {
-    sprite = Math.floor(this.animationTime * 12) % 8
-  }
-
-  this.ctx.save()
-  if (this.flipPlayer)
-    flipHorizontally(this.ctx, x + width / 2);
-
-  this.ctx.drawImage(playerSprites, sprite * width, 0, width, height, x, y, width, height)
-  this.ctx.restore()
-}
-
-CanvasDisplay.prototype.drawActors = function() {
-  this.level.actors.forEach(function(actor) {
-    var width = actor.size.x * scale
-    var height = actor.size.y * scale
-    var x = (actor.position.x - this.viewport.left) * scale
-    var y = (actor.position.y - this.viewport.top) * scale
-    if (actor.type == 'player') {
-      this.drawPlayer(x, y, width, height)
-    } else {
-      var tileX = (actor.type == 'coin' ? 2 : 1) * scale
-      this.ctx.drawImage(otherSprites, tileX, 0, width, height, x, y, width, height);
-    }
-  }, this)
-}
-
 
 // collision and general movement 
 
@@ -371,7 +314,7 @@ Level.prototype.playerTouched = function(type, actor) {
 
 // Input handler 
 
-var arrowCodes = { 37: 'left', 32: 'up', 38: 'up', 39: 'right' }
+var arrowCodes = { 37: 'left', 38: 'up', 39: 'right' }
 
 function trackKeys(codes) {
   var pressed = Object.create(null)
